b5e3ba218f6bf1b889f82907b193dfac81086f37
Lucas Araujo <kirilucas@gmail.com>
2021-09-11 00:54:22 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,41 +1,79 @@
 import './App.css';
 import { useState } from 'react'
-import { CContainer, CRow, CCol } from '@coreui/react';
+import { CContainer, CRow, CCol, CAlert, CModalHeader, CModal, CModalTitle, CModalBody } from '@coreui/react';
 import jsQR from "jsqr";
 import axios from 'axios';
 
 function App() {
 
   const [plainData, setPlainData] = useState('')
+  const [batchId, setBatchId] = useState('')
+  const [potatoTable, setPotatoTable] = useState([])
+
+  const [visibleXL, setVisibleXL] = useState(false)
   const title = 'QRCode data: '
 
-  const onChange = ({ target: { files } }) => {
-    console.log('batata')
+  const onChange = async ({ target: { files } }) => {
+    const image = await createImageBitmap(files[0])
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')
+    const url = `http://localhost:3001/batch/new`
 
-    createImageBitmap(files[0]).then(image => {
-      const canvas = document.createElement('canvas')
-      canvas.width = image.width
-      canvas.height = image.height
+    canvas.width = image.width
+    canvas.height = image.height
+    context.drawImage(image, 0, 0)
 
-      const context = canvas.getContext('2d')
-      context.drawImage(image, 0, 0)
+    const imageData = context.getImageData(0, 0, image.width, image.height)
+    const qrCodeData = jsQR(imageData.data, imageData.width, imageData.height).data
+    const request = await axios.post(url, JSON.parse(qrCodeData), { headers: { 'Content-Type': 'application/json' } })
 
-      const imageData = context.getImageData(0, 0, image.width, image.height)
-      const qrCodeData = jsQR(imageData.data, imageData.width, imageData.height).data
-
-      setPlainData(qrCodeData)
-    
-      const url = `http://localhost:3001/batch/new`      
-      axios.post(url, {qrCodeData})
-
-    })
+    setPlainData(qrCodeData)
+    setBatchId(request.data)
   }
 
+  const onListChange = () => {
+    setVisibleXL(true)
+  }
+
+  function renderToast() {
+    if (batchId) {
+      return (
+        <CAlert color="success" dismissible="true">
+          Batch {batchId} created
+        </CAlert>
+      );
+    }
+}
+
+ async function tableData() {
+    const potato = await axios.get(`http://localhost:3001/batch/list`)
+    setPotatoTable(potato)
+    console.log(potato) 
+  }
+
+  function renderListModal() {
+    if (visibleXL) {
+      return (
+        <CModal size="xl" visible={visibleXL}>
+          <CModalHeader onDismiss={() => setVisibleXL(false)}>
+            <CModalTitle>Batch list</CModalTitle>
+          </CModalHeader>
+          <CModalBody>...</CModalBody>
+
+        </CModal>
+      );
+    }
+  }
+
+  // function onBatata() {
+  //   return axios.get(`http://localhost:3001/batch/list`)
+  // }
+  
+
   return (
-
     <CContainer>
-          <p class="title">{title}{plainData}</p>
-
+      {renderToast()}
+      {renderListModal()}
       <CRow>
         <CCol className="qcolumn">
           <p class="title">New batch</p>
@@ -45,8 +83,8 @@ function App() {
 
         <CCol className="qcolumn">
           <p class="title">List batches</p>
-          <label class="cil-list-rich qicon" for="upload"></label>
-          <input type="file" id="upload" onChange={onChange} />
+          <label class="cil-list-rich qicon" onClick={onListChange}></label>
+          {/* <button type="file" id="upload" onChange={onListChange} /> */}
         </CCol>
 
         <CCol className="qcolumn">
@@ -84,20 +122,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// Difference between
-// const onChange = ({target: {files}}) => {
-//   createImageBitmap(files[0]).then(bmp =>{
-//     console.log(bmp)
-//   })  
-//     // console.log(image.target.files[0])
-//   }
-// and
-// const onChange = (image) => {
-//   createImageBitmap(image.target.files[0]).then(bmp =>{
-//     console.log(bmp)
-//   })  
-//     // console.log(image.target.files[0])
-//   }
